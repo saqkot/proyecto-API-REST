944cfecbdd4b684c255e5f86a07444985c31bc47
saqkot <149250762+saqkot@users.noreply.github.com>
2024-05-27 23:59:22 -0600

    Delete proyecto API REST directory

--- proyecto API REST/src/index.js
+++
@@ -1,6 +0,0 @@
-const app = require('./app');
-
-app.listen(app.get('port'), () =>{
-
-    console.log("servidor escuchando en el puerto", app.get('port'));
-});
--- proyecto API REST/src/modulos/auth/index.js
+++
@@ -1,4 +0,0 @@
-const db = require('../../DB/mysql');
-const ctrl = require('./controlador');
-
-module.exports = ctrl(db);
